a17ed39220e457964812560cc5e309270dbce10b
alainprof <amartel@cstj.qc.ca>
2025-01-22 10:52:32 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AtelierOO_102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atelier en classe du groupe 2C6-102");
+            Console.WriteLine("___________________________________\n");
+
+            AfficherMenu();
+        }
+
+        static void AfficherMenu()
+        {
+            Console.WriteLine(" F: Financier");
+            Console.WriteLine(" H: Humanité");
+            Console.Write("Votre choix:");
+
+            Console.WriteLine("\n\n__________\n Q: Quitter");
+        }
+    }
+}
